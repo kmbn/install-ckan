eb5b7b9b3d15d0e15b60686393a93dbe63511e6c
kmbn <github@nevermindtheumlauts.com>
2018-09-19 22:57:23 +0200

    Fix Solr start

--- install-ckan.sh
+++ install-ckan.sh
@@ -67,8 +67,9 @@ paster make-config ckan /etc/ckan/default/development.ini
 sed -i 's/ckan.site_id = default/ckan.site_id = Default Portal (Development)/g' /etc/ckan/default/development.ini
 sed -i 's/ckan.site_url =/ckan.site_url = http://127.0.0.1:5000/g' /etc/ckan/default/development.ini
 
-# Set up Solr
-sed -i 's/NO_START=.*/NO_START=0\\nJETTY_HOST=127\.0\.0\.1\\nJETTY_PORT=8983/g' /etc/default/jetty9
+# Set up Solr (a fresh install will already have NO_START=0)
+echo "JETTY_HOST=127.0.0.1" >> /etc/default/jetty9
+echo "JETTY_PORT=8983" >> /etc/default/jetty9
 sudo service jetty9 restart
 # Verify that Solr is running
 curl -I http://localhost:8983/solr/
